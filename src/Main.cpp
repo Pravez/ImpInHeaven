688fd2a0e8c7fdff955657117c3e6233e97592a5
Pravez <paul.ravezb@gmail.com>
2017-05-23 11:32:13 +0200

    Added some more details, and keyboard input

--- src/Main.cpp
+++ src/Main.cpp
@@ -6,13 +6,38 @@ extern int main(int argc, char **argv) {
     CHECK_INIT_SDL(SDL_Init(SDL_INIT_VIDEO));
 
 
-
     SDL_Window *sdlWindow = NULL;
     sdlWindow = SDL_CreateWindow("ImpInHeaven", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480,
                                  SDL_WINDOW_SHOWN);
 
-    if(sdlWindow) {
-        SDL_Delay(5000);
+    SDL_Surface *pSurface = NULL;
+    pSurface = SDL_GetWindowSurface(sdlWindow);
+    SDL_Event event;
+    bool end = false;
+    SDL_FillRect(pSurface, NULL, SDL_MapRGB(pSurface->format, 255, 255, 0));
+
+    if (sdlWindow) {
+        while (!end) {
+            while (SDL_PollEvent(&event)) {
+                /* We are only worried about SDL_KEYDOWN and SDL_KEYUP events */
+                switch (event.type) {
+                    case SDL_KEYDOWN:
+                        switch(event.key.keysym.sym){
+                            case SDLK_ESCAPE:
+                                end = true;
+                                break;
+                            default:break;
+                        }
+                        break;
+
+                    default:
+                        break;
+                }
+
+            }
+            SDL_UpdateWindowSurface(sdlWindow);
+        }
+
         SDL_DestroyWindow(sdlWindow);
     }
 
